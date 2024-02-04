5c2a0929cbb29252fd7a2905542d3b3095862a10
nicolegros <nicolaslegros98@gmail.com>
2024-02-04 05:35:00 -0500

    :sparkles: Clicking on logo redirects to `/`

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 
 export function Navbar() {
-  return (
-    <div className="flex justify-between align-middle px-8 pt-8">
-      <Image
-        className="rounded-3xl"
-        src="/green-basket-icon.png"
-        width={60}
-        height={20}
-        alt="Happy farmer"
-      />
-      <div className="bg-chiffon w-full h-16 justify-end align-middle flex">
+    return (
+        <div className="flex justify-between align-middle px-8 pt-8">
+            <a href="/">
+                <Image
+                    className="rounded-3xl"
+                    src="/green-basket-icon.png"
+                    width={60}
+                    height={20}
+                    alt="Happy farmer"
+                />
+            </a>
+            <div className="bg-chiffon w-full h-16 justify-end align-middle flex">
 
-        <button className="mx-4 text-lg hover:text-tangerine"><a href="/">Home</a></button>
-        <button className="mx-4 text-lg hover:text-tangerine"><a href="/about">About</a></button>
-        <button className="mx-4 text-lg hover:text-tangerine"><a href="/map">Map</a></button>
-      </div>
-    </div>
-  )
+                <button className="mx-4 text-lg hover:text-tangerine"><a href="/">Home</a></button>
+                <button className="mx-4 text-lg hover:text-tangerine"><a href="/about">About</a></button>
+                <button className="mx-4 text-lg hover:text-tangerine"><a href="/map">Map</a></button>
+            </div>
+        </div>
+    )
 }
