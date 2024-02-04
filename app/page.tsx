d3343b984f4c8793eacaa76c6d3350936859a79a
nicolegros <nicolaslegros98@gmail.com>
2024-02-04 09:24:12 -0500

    :lipstick: Fixed responsive in `/home`

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="flex h-full lg:flex-row flex-col-reverse justify-center items-center">
             <div className="h-full lg:justify-end  justify-center items-center flex">
-                <div className="flex flex-col justify-end ml-16 xl:w-3/4 w-full">
+                <div className="flex flex-col lg:justify-end justify-center md:ml-16 mx-4 xl:w-3/4 w-full">
                     <h1 className="lg:text-7xl text-5xl text-sacramento max-lg:mt-4">GreenBasket</h1>
                     <p className="py-8 text-sacramento text-md">
                         To fight against food waste and support local farmers, GreenBasket generates a map with all the
@@ -16,7 +16,8 @@ export default function Home() {
                                   text-white
                                   w-48
                                   rounded-full
-                                  p-4"><a href="/map">Find local markets</a></button>
+                                  p-4
+                                  max-md:mb-4"><a href="/map">Find local markets</a></button>
                         <button className="mx-8 text-lg text-sacramento hover:text-tangerine underline"><a
                             href="/about">More information</a></button>
                     </div>
