b6d1f7c94447ffcccd6d3907f6189e2505fd05a6
nicolegros <nicolaslegros98@gmail.com>
2024-02-04 06:42:30 -0500

    :recycle: Fixed ugly buttons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,9 +14,10 @@ export function Navbar() {
             </a>
             <div className="bg-chiffon w-full h-16 justify-end align-middle flex">
 
-                <button className="mx-4 text-lg hover:text-tangerine"><a href="/">Home</a></button>
-                <button className="mx-4 text-lg hover:text-tangerine"><a href="/about">About</a></button>
-                <button className="mx-4 text-lg hover:text-tangerine"><a href="/map">Map</a></button>
+                <button className="text-sacramento mx-4 text-lg hover:text-tangerine"><a href="/">Home</a></button>
+                <button className="text-sacramento mx-4 text-lg hover:text-tangerine"><a href="/about">About</a></button>
+                <button className="text-sacramento mx-4 text-lg hover:text-tangerine"><a href="/map">Map</a></button>
+                <button className="text-tangerine mx-4 text-lg hover:text-sacramento"><a href="/producer">I&apos;m a producer!</a></button>
             </div>
         </div>
     )
